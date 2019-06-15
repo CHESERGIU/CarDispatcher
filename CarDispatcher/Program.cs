62e78c8e88b7cc1ff9ec4cb8e18ee576779c6a1d
CHESERGIU <sergiu.chereches@gmail.com>
2019-06-15 12:51:41 +0300

    struct Car

--- CarDispatcher/Program.cs
+++ CarDispatcher/Program.cs
@@ -4,14 +4,34 @@ namespace CarDispatcher
 {
     class Program
     {
-        enum Priority
+        private enum Priority
         {
             Low,
             High
         }
 
+        private struct Car
+        {
+            public readonly string id;
+            public readonly Priority priority;
+
+            public Car(string id, Priority priority)
+            {
+                this.id = id;
+                this.priority = priority;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Car[] cars =
+            {
+                new Car("Volkswagen", (Priority)1),
+                new Car("Renault",0),
+                new Car("Ford",  Priority.Low)
+            };
+
+            Console.WriteLine(cars[1].priority + " - " + cars[1].id);
             Priority value = (Priority)1;
             Console.WriteLine(value);
             Console.Read();

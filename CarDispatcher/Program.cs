8144e7ae20b53b4beff8468a7c0fb6cd38d61867
CHESERGIU <sergiu.chereches@gmail.com>
2019-06-16 11:19:33 +0300

    Add, Remove, SelectMode of tickets
    Add a ticket in queue, Remove a ticket from first position, SelectMode of tickets after their priority level

--- CarDispatcher/Program.cs
+++ CarDispatcher/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Net.Sockets;
 
 namespace CarDispatcher
 {
-    class Program
+    internal static class Program
     {
         private enum Priority
         {
@@ -26,15 +27,55 @@ namespace CarDispatcher
         {
             Car[] cars =
             {
-                new Car("Volkswagen", (Priority)1),
-                new Car("Renault",0),
-                new Car("Ford",  Priority.Low)
+                new Car("Volkswagen", Priority.Low),
+                new Car("Opel", Priority.Low),
+                new Car("Mercedes", Priority.Low),
+                new Car("Fiesta", Priority.Low)
             };
-
-            Console.WriteLine(cars[1].priority + " - " + cars[1].id);
-            Priority value = (Priority)1;
-            Console.WriteLine(value);
+            
+            var ticket = new Car("Ford", Priority.High);
+            Add(ref cars, ticket);
+            Console.WriteLine(cars[4].priority + " - " + cars[4].id);
+            var ticket1 = new Car("Renault", Priority.High);
+            cars = SelectMode(cars);
+            Remove(ref cars);
+            Add(ref cars, ticket1);
+            cars = SelectMode(cars);
+            Console.WriteLine(cars[0].priority + " - " + cars[0].id);
             Console.Read();
+
         }
+
+        private static Car[] SelectMode(Car[] cars)
+        {
+            for (int i = 0; i < cars.Length; i++)
+            {
+                var max = cars[0];
+                if (max.priority < cars[i].priority)
+                {
+                    max = cars[i];
+                    cars[i] = cars[0];
+                    cars[0] = max;
+                }
+            }
+
+            return cars;
+        }
+
+        private static void Remove(ref Car[] cars)
+        {
+            for (int i = 0; i < cars.Length - 1; i++)
+            {
+                cars[i] = cars[i + 1];
+            }
+            Array.Resize(ref cars, cars.Length - 1);
+        }
+
+        private static void Add(ref Car[] cars, Car ticket)
+        {
+            Array.Resize(ref cars, cars.Length + 1);
+            cars[cars.Length - 1] = ticket;
+        }
+
     }
 }
